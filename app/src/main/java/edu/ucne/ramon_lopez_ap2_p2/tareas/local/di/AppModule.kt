c10e80373c2d6a0e4e24e1febad680326e86b4a7
Danielvi <danielvilopez@gmail.com>
2025-10-29 19:03:42 -0400

    Configura Hilt y agrega permisos necesarios en el manifiesto

---
+++ app/src/main/java/edu/ucne/ramon_lopez_ap2_p2/tareas/local/di/AppModule.kt
@@ -0,0 +1,10 @@
+package edu.ucne.ramon_lopez_ap2_p2.tareas.local.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
