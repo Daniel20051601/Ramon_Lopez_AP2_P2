252fe0a23fed6befc06832885a98e0de44223677
Danielvi <danielvilopez@gmail.com>
2025-11-09 18:52:00 -0400

    Edita los restornos de las funciones para retornar Gasto en vez de GastoResponse

--- app/src/main/java/edu/ucne/ramon_lopez_ap2_p2/domain/repository/GastoRepository.kt
+++ app/src/main/java/edu/ucne/ramon_lopez_ap2_p2/domain/repository/GastoRepository.kt
@@ -1,13 +1,13 @@
 package edu.ucne.ramon_lopez_ap2_p2.domain.repository
 
 import edu.ucne.ramon_lopez_ap2_p2.data.remote.Resource
-import edu.ucne.ramon_lopez_ap2_p2.data.remote.dto.GastoRequest
 import edu.ucne.ramon_lopez_ap2_p2.data.remote.dto.GastoResponse
+import edu.ucne.ramon_lopez_ap2_p2.domain.model.Gasto
 import kotlinx.coroutines.flow.Flow
 
 interface GastoRepository {
-    suspend fun getGastos(): Flow<List<GastoResponse>>
-    suspend fun postGasto(gastoRequest: GastoRequest): Resource<GastoResponse?>
-    suspend fun putGasto(id: Int, gastoRequest: GastoRequest): Resource<Unit>
-    suspend fun getGastoById(id: Int): Resource<GastoResponse>
+    suspend fun getGastos(): Flow<List<Gasto>>
+    suspend fun postGasto(gasto: Gasto): Resource<Gasto?>
+    suspend fun putGasto(id: Int, gasto: Gasto): Resource<Unit>
+    suspend fun getGastoById(id: Int): Resource<Gasto?>
 }
